b26cdeed24d2d58f2ec29a495eb5bccfd1c34d39
saldivarAdiii <hannajanemanlangit33@gmail.com>
2024-04-19 08:36:12 +0800

    added new textfile todolist.tsx and todolist.css

---
+++ src/pages/Todolist.tsx
@@ -0,0 +1,77 @@
+import {
+  //Initial Components
+  IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonThumbnail,
+  IonButton
+} from '@ionic/react';
+import './home.css';
+
+// CSS
+//import './profile.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Home</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonCard>
+      <IonCardHeader>
+        <IonCardTitle>Adi's Application</IonCardTitle>
+        <IonCardSubtitle></IonCardSubtitle>
+      </IonCardHeader>
+      <IonCardContent>
+        <IonList>
+          <IonItem>
+            <IonThumbnail slot="start">
+              <img alt="Silhouette of mountains" src="../src/assets/img/click.png" />
+            </IonThumbnail>
+            <IonButton href='/Click'>Click Counter</IonButton>
+          </IonItem>
+
+          <IonItem>
+            <IonThumbnail slot="start">
+              <img alt="Silhouette of mountains" src="../src/assets/img/calculator.png" />
+            </IonThumbnail>
+            <IonButton href='/calculator'>Calculator</IonButton>
+          </IonItem>
+
+          <IonItem>
+            <IonThumbnail slot="start">
+              <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
+            </IonThumbnail>
+            <IonLabel>Blank</IonLabel>
+          </IonItem>
+
+          <IonItem lines="none">
+            <IonThumbnail slot="start">
+              <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
+            </IonThumbnail>
+            <IonLabel>Blank</IonLabel>
+          </IonItem>
+        </IonList>
+      </IonCardContent>
+    </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+//
+export default Home;
