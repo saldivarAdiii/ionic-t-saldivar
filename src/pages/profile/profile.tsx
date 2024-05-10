77937d5d529b90f731c76c538fb2e1d624d44d54
saldivarAdiii <blessedadiii@gmail.com>
2024-05-10 10:48:42 +0800

    Added text on profile.tsx

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -20,20 +20,21 @@ const Profile: React.FC = () => {
 
         </IonHeader>
         <IonCard>
-      <img alt="Silhouette of mountains" src="https://github.com/saldivarAdiii/ionic-t-saldivar/blob/main/src/assets/img/Adiblessed.jpeg" />
+      <img alt="Silhouette of mountains" src="https://raw.githubusercontent.com/saldivarAdiii/ionic-t-saldivar/main/src/assets/img/Adiblessed.jpeg" />
       <IonCardHeader>
         <IonCardTitle>adiii</IonCardTitle>
         <IonCardSubtitle>blessed</IonCardSubtitle>
       </IonCardHeader>
 
-      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+      <IonCardContent>She was one of the women who had been "healed of evil spirits and ifirmities" and became a close associate of Christ.</IonCardContent>
 
       <IonButton id="present-alert">Click Me</IonButton>
         <IonAlert
           trigger="present-alert"
-          header="A Short Title Is Best"
-          subHeader="A Sub Header Is Optional"
-          message="A message should be a short, complete sentence."
+          header="She's 21"
+          subHeader="Well done is better than well said :)"
+          message="there are no regrets in life, just lessons
+          -Jennifer Aniston"
           buttons={['Action']}
         ></IonAlert>
 
